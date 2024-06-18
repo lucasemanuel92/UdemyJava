e939eb15420684de8b53302cadcd65f5fe4ce55c
lucasEmanuel <lucas.emanuel@kissolutions.tech>
2024-06-18 16:52:11 -0300

    new file

---
+++ fundamentos/teoria/TipoString.java
@@ -0,0 +1,22 @@
+package fundamentos.teoria;
+
+public class TipoString {
+    public static void main(String[] args) {
+        System.out.println("Olá pessoal".charAt(0));
+
+        String s = "Boa tarde";
+        // Ao usar qualquer método usado s = s.QualquerCoisa, eu estou substituindo o valor e não alterando.
+        System.out.println(s.concat("!!!"));
+        System.out.println(s + "!!!");
+        System.out.println(s.startsWith("Boa")); // Tipo booleano, idicando verdadeiro ou falso
+        System.out.println(s.length()); // Pode indicar um valor
+        System.out.println(s.equals("boa tarde")); // Compara valores e pode se combinado como  equalsIgnoreCase e outras
+
+        var nome = "Pedro";
+        var sobrenome = "Santos";
+        var nomeCompleto = nome.concat(" " + sobrenome);
+        System.out.println(nomeCompleto);
+
+        System.out.printf("Nome: %s %s", nome, sobrenome); // %s ou %d... é usado como formatação para não precisar colocar tudo na frase
+    }
+}
