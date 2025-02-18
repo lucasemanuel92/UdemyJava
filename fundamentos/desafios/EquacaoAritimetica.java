ee4fa8a9fc1ccc9831ed5a7f5a43643351d2148e
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-02-18 14:39:11 -0300

    Equação Aritimética - Desafio

---
+++ fundamentos/desafios/EquacaoAritimetica.java
@@ -0,0 +1,23 @@
+package fundamentos.desafios;
+
+public class EquacaoAritimetica {
+    public static void main(String[] args) {
+
+        double a = 6 * (3 + 2);
+        double b = 3 * 2;
+        double c = Math.pow(a, 2) / b;
+
+        double d = (double) ((1 - 5) * (2 - 7)) / 2;
+        double e = Math.pow(d, 2);
+
+        double f = c - e;
+        double g = Math.pow(f, 3);
+
+        double h = Math.pow(10, 3);
+
+        double results = g / h;
+        System.out.println(results);
+
+
+    }
+}
