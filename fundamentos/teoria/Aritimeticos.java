622cdc95491899318e371297566dfba1b0a54917
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-02-18 14:24:41 -0300

    Operadores Aritiméticos

---
+++ fundamentos/teoria/Aritimeticos.java
@@ -0,0 +1,22 @@
+package fundamentos.teoria;
+
+public class Aritimeticos {
+    public static void main(String[] args) {
+        // Posso colocar direto no System
+        System.out.println(2+3);
+
+        var x = 34.56;
+        double y = 2.2;
+        System.out.println(x - y);
+
+        int a = 8;
+        int b = 3;
+        // Nesse caso a divisão de inteiros dará um inteiro
+        System.out.println(a/b);
+
+        // % entrega o resto
+        System.out.println(a % b);
+
+
+    }
+}
