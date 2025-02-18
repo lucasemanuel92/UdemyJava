fdb27bcfd3a24ff5f981bc327c71604640fc1f34
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-02-18 15:06:40 -0300

    Desafio Operadores Lógicos

---
+++ fundamentos/desafios/Trabalhos.java
@@ -0,0 +1,16 @@
+package fundamentos.desafios;
+
+public class Trabalhos {
+    public static void main(String[] args) {
+        boolean trabalho1 = false;
+        boolean trabalho2 = false;
+
+        if (trabalho1 && trabalho2 == true){
+            System.out.println("TV de 50 e sorvete");
+        } else if (trabalho1 != trabalho2) {
+            System.out.println("TV de 32 e sorvete");
+        } else {
+            System.out.println("Ninguem vai ganhar nada");
+        }
+    }
+}
